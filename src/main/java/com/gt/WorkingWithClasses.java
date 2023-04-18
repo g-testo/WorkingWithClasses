4909abcb712d671af5c0e4d79f09521f28186893
Gregory Testo <gt@gregorytesto.com>
2023-04-18 15:43:38 -0400

    created a constuctor and added a name getter

--- src/main/java/com/gt/WorkingWithClasses.java
+++ src/main/java/com/gt/WorkingWithClasses.java
@@ -2,13 +2,16 @@ package com.gt;
 
 public class WorkingWithClasses {
     public static void main(String[] args){
-        Person greg = new Person();
-        greg.name = "Greg";
-        greg.age = 21;
-        greg.birthday = "04-10-1980";
-        greg.hasTattoo = true;
+        Person greg = new Person("Greg", 21, "04-10-1980", true, "000-00-0000");
 
-        System.out.println(greg.name);
+
+
+//        greg.name = "Greg";
+//        greg.age = 21;
+//        greg.birthday = "04-10-1980";
+//        greg.hasTattoo = true;
+
+//        System.out.println(greg.ssn);
 
 //        Person braynel = new Person();
 //        braynel.name = "Braynel";
@@ -19,12 +22,23 @@ public class WorkingWithClasses {
 
 class Person {
     // Properties
-    String name;
-    int age;
-    String birthday;
-    boolean hasTattoo;
+    private String name;
+    private int age;
+    private String birthday;
+    private boolean hasTattoo;
+    private String ssn;
 
-    // Constructor
+    // Constructor method
+    public Person(String name, int age, String birthday, boolean hasTattoo, String ssn){
+        this.name = name;
+        this.age = age;
+        this.birthday = birthday;
+        this.hasTattoo = hasTattoo;
+        this.ssn = ssn;
+    }
 
-    // Methods
+    // Methods- Getters and Setters
+    public String getName(){
+        return this.name;
+    }
 }
