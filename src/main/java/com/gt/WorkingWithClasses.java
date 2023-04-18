bb18ef62a7e0dfeb71b63a74a88d5ac62c479c80
Gregory Testo <gt@gregorytesto.com>
2023-04-18 15:15:01 -0400

    created a person class

--- src/main/java/com/gt/WorkingWithClasses.java
+++ src/main/java/com/gt/WorkingWithClasses.java
@@ -2,6 +2,29 @@ package com.gt;
 
 public class WorkingWithClasses {
     public static void main(String[] args){
+        Person greg = new Person();
+        greg.name = "Greg";
+        greg.age = 21;
+        greg.birthday = "04-10-1980";
+        greg.hasTattoo = true;
+
+        System.out.println(greg.name);
+
+//        Person braynel = new Person();
+//        braynel.name = "Braynel";
+
 
     }
+}
+
+class Person {
+    // Properties
+    String name;
+    int age;
+    String birthday;
+    boolean hasTattoo;
+
+    // Constructor
+
+    // Methods
 }
